570731029ba874b2382e317c065c64e286646f7c
Mike Helmick <mike.helmick@gmail.com>
2017-11-05 21:59:47 -0800

    load dictionary from file

--- main/main.go
+++ main/main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+  "bufio"
 	"fmt"
 	"github.com/mikehelmick/bogglego/trie"
+  "os"
 )
 
-func main() {
-	fmt.Println("Hello, world!")
+// Simple error check
+func check(e error) {
+  if e != nil {
+    panic(e);
+  }
+}
 
-	dictionary := trie.New()
-	fmt.Println(dictionary)
+func loadDictionary(filename string, dict *trie.Trie) {
+  fmt.Printf("Loading dictionry from %s\n", filename);
+  dat, err := os.Open(filename);
+  check(err);
+  defer dat.Close();
+
+  scanner := bufio.NewScanner(dat)
+  scanner.Split(bufio.ScanLines)
+
+  var wordCount int32 = 0;
+  for scanner.Scan() {
+    dict.AddWord(scanner.Text());
+    wordCount++;
+    if wordCount % 1000 == 0 {
+      fmt.Print(".");
+    }
+  }
+  fmt.Printf("Loaded %v words.\n", wordCount);
+}
+
+func main() {
+  args := os.Args[1:]
+  if len(args) != 1 {
+    panic("You must provide a filename to load.");
+  }
+  dict := trie.New();
+  loadDictionary(args[0], dict);
 }
